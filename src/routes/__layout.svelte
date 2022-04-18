17405a2c658406df2d9d13bbfdb9300eb730efd8
Sage Raflik <neonfuz@gmail.com>
2022-04-18 16:45:37 -0500

    Hover preview idea

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,4 +1,4 @@
-<svelte:head>
-    <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css">
-</svelte:head>
+<script>
+ import 'milligram';
+</script>
 <slot/>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,13 +2,30 @@
  const foo = [
      {
          name: "MiniPlayer in SideBar",
-         href: "https://gist.github.com/neonfuz/6d3790334983b87b638ddecf84d0ab09"
+         href: "https://gist.github.com/neonfuz/6d3790334983b87b638ddecf84d0ab09",
+         id:   "youtubeMiniplayerSidebar"
      },
      {
          name: "Quick Playlist Button",
-         href: "https://gist.github.com/neonfuz/ea14fe2ad32c4caa860f36bb521b9a60"
+         href: "https://gist.github.com/neonfuz/ea14fe2ad32c4caa860f36bb521b9a60",
+         id:   "youtubePlaylistButton"
      },
  ];
+ let tabId = '';
+ function preview(node, content) {
+     function onHover() {
+         tabId = content;
+     }
+     node.addEventListener('mouseenter', onHover);
+     return {
+         destroy() {
+             node.removeEventListener('mouseenter', onHover);
+         }
+     }
+ }
+ function tab(name, tabId) {
+     return tabId === name ? 1 : 0;
+ }
 </script>
 
 <header class="container">
@@ -16,24 +33,82 @@
 </header>
 
 <main class="container">
-    <section>
-        <h3>Resume</h3>
-        <ul>
-            <li>
-                <a href="https://neonfuz.github.io/resume/resume.pdf">PDF</a>
-            </li>
-        </ul>
-    </section>
-    <section>
-        <h3>Youtube improvements</h3>
-        <ul>
-            {#each foo as {name, href}}
-            <li>
-                <a href={href}>
-                    {name}
-                </a>
-            </li>
-            {/each}
-        </ul>
-    </section>
+    <div class="row">
+        <div class="column">
+            <section>
+                <h3>Links</h3>
+                <ul>
+                    <li>
+                        <a href="https://neonfuz.github.io/resume/resume.pdf"
+                           use:preview={"resume"}>
+                            Resume
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/neonfuz"
+                           use:preview={"github"}>
+                            Github
+                        </a>
+                    </li>
+                </ul>
+            </section>
+            <section>
+                <h3>Youtube improvements</h3>
+                <ul>
+                    {#each foo as {name, href, id}}
+                        <li>
+                            <a href={href} use:preview={id}>
+                                {name}
+                            </a>
+                        </li>
+                    {/each}
+                </ul>
+            </section>
+        </div>
+        <div class="column preview">
+            <div style:opacity={tab('resume', tabId)}
+                               style:width="100%">
+                <div class="pdf-wrap">
+                    <a href="https://neonfuz.github.io/resume/resume.pdf"></a>
+                    <iframe
+                        title="resume"
+                        frameborder="0"
+                        src="https://neonfuz.github.io/resume/resume.pdf" />
+                </div>
+            </div>
+            <div style:opacity={tab('github', tabId)}>
+                <img alt="Github" src="/github.png"/>
+            </div>
+            <div style:opacity={tab('youtubePlaylistButton', tabId)}>
+                <img alt="Youtube Quick Playlist Buttons" src="/youtube-playlist.png" />
+            </div>
+        </div>
+    </div>
 </main>
+
+<style>
+ .preview {
+     position: relative;
+ }
+ .preview > * {
+     position: absolute;
+     top: 0;
+     transition: opacity .4s;
+ }
+ .pdf-wrap {
+     display: block;
+     position: relative;
+     padding-bottom: 129.41%;
+ }
+ .pdf-wrap a {
+     display: block;
+     position: absolute;
+     inset: 0;
+     z-index: 1;
+ }
+ iframe {
+     position: absolute;
+     width: 100%;
+     height: 100%;
+ }
+</style>
